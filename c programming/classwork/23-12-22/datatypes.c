5e0e8cd1d0d573a900b8a766efc42cd3b25cbf1e
Kaushikhirani <120399459+Kaushikhirani@users.noreply.github.com>
2022-12-23 10:56:04 +0530

    23-12-22

---
+++ c programming/classwork/23-12-22/datatypes.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+ void main()
+ {
+ 	int no=29;
+ 	char ch='u';
+ 	float per=58.78;
+ 	double stock=36374748.3746456537;;
+ 	char name[10]="TOPS TEC";
+ 	char mobile[12]="0751021778";
+ 	 
+ 	 printf("%d\n",no);
+ 	 printf("%c\n",ch);
+ 	 printf("%f\n",per);
+ 	 printf("%lf\n",stock);
+ 	 printf("%s\n",name);
+ 	 printf("%s\n",mobile);
+ }
