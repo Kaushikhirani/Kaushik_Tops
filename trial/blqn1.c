c675f296d4f78f62f4ea34381c78c008ef5dc911
Kaushikhirani <120399459+Kaushikhirani@users.noreply.github.com>
2024-02-10 15:33:32 +0530

    Add files via upload

---
+++ trial/blqn1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+ {
+ int name,birthDate,age,address;
+    
+    printf("a. Name : Hirani Kaushik\n", name);
+    printf("b. Birth : 19-04-2002\n", birthDate);
+    printf("c. Age: 22 years\n", age);
+    printf("d. Address: Naranpura, Ahmedabad", address);
+
+    return 0;
+}
